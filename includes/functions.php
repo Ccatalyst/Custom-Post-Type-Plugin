c648fc4e3027d4af6ba1e32bacd48c0cdeb5a332
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-24 17:58:42 -0700

    working on adding the custom fields needed for unit post type, and adding custom column

--- includes/functions.php
+++ includes/functions.php
@@ -37,20 +37,61 @@ function add_unit_post_type() {
 
 		$args = array(
 			'labels' => array(
-				'name' => 'Units',
-				'singular_name' => 'Unit'
+				'name' => 'unit',
+				'singular_name' => 'unit'
 			)
 			, 'public' => true,
 			'has_archive' => true,
-			'rewrite' => array( 'slug' => 'units' ),
+			'rewrite' => array( 'slug' => 'unit' ),
 		);
 		register_post_type( 'unit_post_type', $args );
 	}
-	function custom_columns( $columns ) {
+	function custom_fields() {
+
+		register_meta( 'unit', 'asset_id', [ 
+			'type' => 'string',
+			'description' => 'Custom field for asset ID',
+			'single' => true,
+			'show_in_rest' => true,
+		] );
+		register_meta( 'unit', 'building_id', [ 
+			'type' => 'string',
+			'description' => 'Custom field for building ID',
+			'single' => true,
+			'show_in_rest' => true,
+		] );
+		register_meta( 'unit', 'floor_id', [ 
+			'type' => 'string',
+			'description' => 'Custom field for floor ID',
+			'single' => true,
+			'show_in_rest' => true,
+		] );
+		register_meta( 'unit', 'floor_plan_id', [ 
+			'type' => 'string',
+			'description' => 'Custom field for floor plan ID',
+			'single' => true,
+			'show_in_rest' => true,
+		] );
+		register_meta( 'unit', 'area', [ 
+			'type' => 'number',
+			'description' => 'Custom field for area',
+			'single' => true,
+			'show_in_rest' => true,
+		] );
+	}
+
+	// functions add custom column to hold floor_plan_id field
+	function floor_plan_column_header( $columns ) {
 		$columns['floor_plan_id'] = "Floor Plan ID";
 		return $columns;
 	}
-	;
+	function floor_plan_column_content( $column, $post_id ) {
+		if ( $column == 'floor_plan_custom_header' ) {
+			echo get_post_meta( $post_id, 'floor_plan_id' );
+		}
+	}
+
+
 	add_action( "init", "unit_post_type" );
-	add_filter( 'manage_unit_post_type_columns', "custom_columns", 10, 2 );
+	add_filter( 'manage_unit_posts_columns', "floor_plan_column_header", 10, 2 );
 }
