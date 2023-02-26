fbb2ac97e1596542a849350c60a9279bad09c60f
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-26 13:06:07 -0700

    with the backend stuff 'done', Starting work on shortcode for the unit posts.

---
+++ includes/shortcode/shortcode.php
@@ -0,0 +1,4 @@
+<?php
+
+
+// Query all unit posts, and run a while loop over it. Each output will be a <li> with the various datapoints of the unit post in it. It should be ordered by area, so the return can be split into two categories (>1, ==1).
