b0644182fda406755b3644c4ef53c41dbe54f792
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-26 00:49:37 -0700

    added additional custom field unit_number, changed name of title in list to Unit Number, reordered column in list to have Floor Plan ID column in the center. Altered folder structure to be a little further from actual chaos

--- includes/functions.php
+++ includes/functions.php
@@ -38,12 +38,20 @@ function add_unit_post_type() {
 		$args = array(
 			'labels' => array(
 				'name' => 'Units',
-				'singular_name' => 'Unit'
+				'singular_name' => 'Unit',
+				'add_new' => 'Add New Unit',
+				'add_new_item' => 'Add New Unit',
+				'edit_item' => 'Edit Unit',
+				'view_item' => 'View Unit',
+				'search_items' => 'Search Units',
+				'menu_name' => 'Units',
+
 			)
 			, 'public' => true,
 			'show_in_menu' => 'edit.php',
 			'has_archive' => true,
 			'rewrite' => array( 'slug' => 'unit' ),
+			'title' => 'Unit Number'
 		);
 
 		register_post_type( 'unit', $args );
@@ -54,28 +62,28 @@ function add_unit_post_type() {
 
 		register_meta( 'unit', 'floor_id', [ 
 			'type' => 'string',
-			'description' => 'Custom field for floor ID',
+			'description' => 'Custom field for the floor ID',
 			'single' => true,
 			'show_in_rest' => true,
 		] );
 
 		register_meta( 'unit', 'asset_id', [ 
 			'type' => 'string',
-			'description' => 'Custom field for asset ID',
+			'description' => 'Custom field for the asset ID',
 			'single' => true,
 			'show_in_rest' => true,
 		] );
 
 		register_meta( 'unit', 'building_id', [ 
 			'type' => 'string',
-			'description' => 'Custom field for building ID',
+			'description' => 'Custom field for the building ID',
 			'single' => true,
 			'show_in_rest' => true,
 		] );
 
 		register_meta( 'unit', 'floor_plan_id', [ 
 			'type' => 'string',
-			'description' => 'Custom field for floor plan ID',
+			'description' => 'Custom field for the floor plan ID',
 			'single' => true,
 			'show_in_rest' => true,
 		] );
@@ -83,6 +91,12 @@ function add_unit_post_type() {
 		register_meta( 'unit', 'area', [ 
 			'type' => 'number',
 			'description' => 'Custom field for area',
+			'single' => true,
+			'show_in_rest' => true,
+		] );
+		register_meta( 'unit', 'unit_number', [ 
+			'type' => 'string',
+			'description' => 'Custom field for the unit number',
 			'single' => true,
 			'show_in_rest' => true,
 		] );
@@ -99,9 +113,25 @@ function add_unit_post_type() {
 			echo get_post_meta( $post_id, 'floor_plan_id', true );
 		}
 	}
+	// Changes the name of the title column to 'Unit Number'
+	function title_column_name_change( $columns ) {
+		$columns['title'] = 'Unit Number';
+		return $columns;
+	}
+	// changes the order of the columns on the admin list table 
+	function reorder_table_columns( $columns ) {
+		unset( $columns['date'] );
+		$columns['title'] = 'Unit Number';
+		$columns['floor_plan_id'] = 'Floor Plan Id';
+		$columns['date'] = 'Date';
+
+		return $columns;
+	}
+
 
 	// Add the custom column to the post type
 	add_filter( 'manage_unit_posts_columns', 'floor_plan_column_header', 10 );
 	add_action( 'manage_unit_posts_custom_column', 'floor_plan_column_content', 10, 2 );
-
+	add_filter( 'manage_unit_posts_columns', 'title_column_name_change' );
+	add_filter( 'manage_unit_posts_columns', 'reorder_table_columns' );
 }
