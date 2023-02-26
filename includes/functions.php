884cbd85806e178887f816463ecafe3a487707a9
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-25 22:01:31 -0700

    long walk to add an edit box to a custom field. Added notes on the function to better understand what is happening.

--- includes/functions.php
+++ includes/functions.php
@@ -98,38 +98,11 @@ function add_unit_post_type() {
 		if ( $column == 'floor_plan_id' ) {
 			echo get_post_meta( $post_id, 'floor_plan_id', true );
 		}
-
 	}
+
 	// Add the custom column to the post type
 	add_filter( 'manage_unit_posts_columns', 'floor_plan_column_header', 10 );
 	add_action( 'manage_unit_posts_custom_column', 'floor_plan_column_content', 10, 2 );
-
-
-}
-
-function test_unit_post() {
-	$post_data = array(
-		'post_type' => 'unit',
-		'post_title' => '01100',
-		'post_content' => 'test post',
-		'post_status' => 'publish',
-		'meta_input' => array(
-			'floor_id' => '4358',
-			'asset_id' => '1273',
-			'building_id' => '7783',
-			'floor_plan_id' => '4358',
-			'area' => 879,
-
-		)
-	);
-	$post_id = wp_insert_post( $post_data, true );
-
-
-	if ( $post_id ) {
-		echo "Successfully added post";
-	} else {
-		echo "Something has gone wrong";
-	}
 
 
 }
--- includes/unit-admin-page.php
+++ includes/unit-admin-page.php
@@ -1,55 +1,67 @@
 <?php
-// TODO: create a button that will run the api query function. When the data is returned, it needs to be looped over and the data turned into unit posts via wp_insert_post WP function https://developer.wordpress.org/reference/functions/wp_insert_post/  Testing is needed to confirm the custom fields are a part of the unit post meta
-function unit_admin_page_callback() {
-	?>
-	<div class="wrap">
-		<h2>Welcome To My Plugin</h2>
-	</div>
-	<?php
-}
+// TODO: create a button that will run the api query function. When the data is returned, it needs to be looped over and the data turned into unit posts via wp_insert_post WP function https://developer.wordpress.org/reference/functions/wp_insert_post/  
 
-// TODO:Move the callbacks to their own files, they're going to get larger with the forms and such. For the edit page of the unit post type, extend the WP_Post class and overwrite what is needed, such as the url to the custom edit page.
-function unit_admin_subpage_callback() {
-	$args = array(
-		'post_type' => 'unit',
-		'post_status' => 'publish',
-		'posts_per_page' => -1,
-	);
-	$query = new WP_Query( $args );
-	if ( $query->have_posts() ) :
-		?>
-		<table class="wp-list-table widefat fixed striped">
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Title</th>
-					<th>Author</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php while ( $query->have_posts() ) :
-					$query->the_post(); ?>
-					<tr>
-						<td>
-							<?php the_ID(); ?>
-						</td>
-						<td>
-							<?php the_title(); ?>
-						</td>
-						<td>
-							<?php the_author(); ?>
-						</td>
-					</tr>
-				<?php endwhile; ?>
-			</tbody>
-		</table>
-		<?php
-	endif;
-	wp_reset_postdata();
-}
+
+// TODO: Add inputs for all custom fields on unit edit page via add_meta_box
+
+
 function unit_admin_pages() {
-	add_menu_page( 'Engrain Units', 'Units Admin', 'manage_options', 'unit-admin-page', 'unit_admin_page_callback', '', 6 );
-
-	// add_submenu_page( 'unit-admin-page', 'Unit edit', 'Unit Edit', 'manage_options', 'edit.php?post_type=unit', 'unit_admin_subpage_callback' );
-
+	add_menu_page( 'Engrain Units', 'Units', 'manage_options', 'unit-admin-page', 'unit_admin_page_callback', '', 6 );
 }
+
+function floor_id_edit() {
+
+
+	// Okay, this was a trip. shout out to Mo Ismailzai on the Mugo Web Blog for his article on complex fields to custom post types https://www.mugo.ca/Blog/Adding-complex-fields-to-WordPress-custom-post-types
+
+	// add_floor_id_meta_box creates the box to edit the floor_id in the edit page for the post. The callback in the third argument is below
+	function add_floor_id_meta_box() {
+		add_meta_box(
+			'floor_id_meta_box',
+			'Floor ID',
+			'floor_id_meta_box_callback',
+			'unit'
+		);
+	}
+
+	/* 
+	callback function for the add_meta_box WP function
+	
+	*/
+	function floor_id_meta_box_callback( $post ) {
+		// this adds a security nonce(number used once) to the metabox. Adding a conditional within the save function that checks to see if the number that's passed to the wp_verify_nonce HTTP POST method.
+		// The basename just throws out all the leading filepath up to the last dir in the filepath. 
+		wp_nonce_field( basename( __FILE__ ), 'floor_id_meta_box_nonce' );
+
+		// Assigns the current post's floor_id value to a variable. We don't want it in an array or anything, so we add the $single = true third argument.
+		$floor_id = get_post_meta( $post->ID, 'floor_id', true );
+		// The actual HTML for the input. Takes the floor_id variable above and plops it in there using the esc_attr WP method
+		echo '<label for="floor_id_field">Floor ID</label>';
+		echo '<input type="text" id="floor_id_field" name="floor_id_field" value="' . esc_attr( $floor_id ) . '">';
+	}
+
+	// this actually allows for saving the above input value when it's changed. 
+	function save_floor_id_meta_box( $post_id ) {
+		// checks to see if the nonce created above exists, or if it can't verify the nonce. If either are false, it exits the function
+		if ( ! isset( $_POST['floor_id_meta_box_nonce'] ) || ! wp_verify_nonce( $_POST['floor_id_meta_box_nonce'], basename( __FILE__ ) ) ) {
+			return;
+		}
+		// if 'DOING_AUTOSAVE' is defined and it's true, exit the function. Variable is set by wp_autosave function
+
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+			return;
+		}
+		// If the currently logged in user can't edit posts, exit the function.
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+		// If the floor_id_field exists, run the update_post_meta WP function. The new value is sanitized to remove tags, extra whitespace, etc. 
+		if ( isset( $_POST['floor_id_field'] ) ) {
+			update_post_meta( $post_id, 'floor_id', sanitize_text_field( $_POST['floor_id_field'] ) );
+		}
+	}
+
+	add_action( 'add_meta_boxes_unit', 'add_floor_id_meta_box' );
+	add_action( 'save_post_unit', 'save_floor_id_meta_box' );
+}
+floor_id_edit();
