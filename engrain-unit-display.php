1494c579764d9031bbeb35de87ca9244610e13e3
Matthew Todor <unclejunglegeorge@gmail.com>
2023-02-25 23:07:46 -0700

    removed top-level menu item for Units, instead put it under Posts.

--- engrain-unit-display.php
+++ engrain-unit-display.php
@@ -7,6 +7,13 @@ Version: 1.0
 Author: Matthew Todor
 */
 
+/*
+WISHLIST: to be addressed once assessment requirements are met
+-Make Floor Plan ID column sortable
+-Alter unit list page to be structured more like the category/tag pages, with the "create new" fields right on the page.
+*/
+
+
 include 'includes/functions.php';
 include 'includes/unit-admin-page.php';
 // Creating custom post type 'Unit'
@@ -14,4 +21,4 @@ add_action( "init", "add_unit_post_type" );
 
 
 // setting up the admin pages for the unit type
-add_action( 'admin_menu', 'unit_admin_pages' );
+// add_action( 'admin_menu', 'unit_admin_pages' );
